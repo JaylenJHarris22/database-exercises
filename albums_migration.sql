336f2e345c90374014627a78f9e4bc8ed569e76d
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-26 09:10:19 -0500

    finished the select exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE albums(
     id INT UNSIGNED primary key AUTO_INCREMENT,
     artist VARCHAR(50),
     name VARCHAR(50),
-    release_date DATE,
+    release_date INT,
     sales FLOAT,
     genre VARCHAR(100)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,95 @@
+USE codeup_test_db;
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (51.2, 'Michael Jackson', 'Thriller', 1982, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (30.1, 'AC/DC', 'Back in Black', 1980, 'Hard rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (28.7, 'Whitney Houston', 'The Bodyguard', 1992, 'R&B');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (24.8, 'Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (41.2, 'Eagles', 'Their Greatest Hits(1971-1975)', 1976, 'Country');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22, 'Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (31.8, 'Eagles', 'Hotel California', 1976, 'Soft rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (30.4, 'Shania Twain', 'Come on Over', 1997, 'Country');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (29.3, 'Fleetwood Mac', 'Rumors', 1977, 'Soft rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22.1, 'Bee Gees', 'Saturday Night Fever', 1977, 'Disco');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (30.4, 'Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (21.2, 'Michael Jackson'), 'Bad', 1987, 'Pop';
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (25.4, 'Alanis Morissette', 'Jagged Little Pill', 1995, 'Alternative rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (24.1, 'Various Artist', 'Dirty Dancing', 1987, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (21.1, 'Celine Dion', 'Falling Into You', 1996, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (27.1, 'Adele', '21', 2011, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (23.2, 'The Beatles', '1', 2000, 'Rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22.7, 'Metallica', 'Metallica', 1991, 'Heavy metal');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (20.5, 'Celine Dion', 'Let''s Talk About Love', 1997, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22.9, 'Bob Marley', 'Legend: The Best of Bob Marley & The Wailers', 1984, 'Reggae');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22.8, 'Guns N Roses', 'Appetite for Destruction', 1987, 'Hard rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (22, 'Bruce Springsteen', 'Born in the U.S.A.', 1984, 'Heartland rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (21.6, 'ABBA', 'Gold: Greatest Hits', 1992, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (21.1, 'Dire Straits', 'Brothers in Arms', 1985, 'Roots rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (20.8, 'Santana', 'Supernatural', 1999, 'Latin Rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (19.8, 'Madonna', 'The Immaculate Collection', 1990, 'Pop');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (18.9, 'Pink Floyd', 'The Wall', 1979, 'Progressive rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (18.3, 'The Beatles', 'Sgt Pepper''s Lonely Hearts Club Band', 'Rock');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (17.0, 'Michael Jackson', 'Dangerous', 1991, 'New jack swing');
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES (16.9, 'The Beatles', 'Abbey Road', 1969, 'Rock');
+
+INSERT INTO albums(sales, artist, name, release_date, genre)
+VALUES
+        (51.2, 'Michael Jackson', 'Thriller', 1982, 'Pop'),
+        (30.1, 'AC/DC', 'Back in Black', 1980, 'Hard rock'),
+        (28.7, 'Whitney Houston', 'The Bodyguard', 1992, 'R&B'),
+        (24.8, 'Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock'),
+        (41.2, 'Eagles', 'Their Greatest Hits(1971-1975)', 1976, 'Country'),
+        (22, 'Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock'),
+        (31.8, 'Eagles', 'Hotel California', 1976, 'Soft rock'),
+        (30.4, 'Shania Twain', 'Come on Over', 1997, 'Country'),
+        (29.3, 'Fleetwood Mac', 'Rumors', 1977, 'Soft rock'),
+        (22.1, 'Bee Gees', 'Saturday Night Fever', 1977, 'Disco'),
+        (30.4, 'Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock'),
+        (21.2, 'Michael Jackson', 'Bad', 1987, 'Pop'),
+        (25.4, 'Alanis Morissette', 'Jagged Little Pill', 1995, 'Alternative rock'),
+        (24.1, 'Various Artist', 'Dirty Dancing', 1987, 'Pop'),
+        (21.1, 'Celine Dion', 'Falling Into You', 1996, 'Pop')
+        (27.1, 'Adele', '21', 2011, 'Pop'),
+        (23.2, 'The Beatles', '1', 2000, 'Rock'),
+        (22.7, 'Metallica', 'Metallica', 1991, 'Heavy metal'),
+        (20.5, 'Celine Dion', 'Let''s Talk About Love', 1997, 'Pop'),
+        (22.9, 'Bob Marley', 'Legend: The Best of Bob Marley & The Wailers', 1984, 'Reggae'),
+        (22.8, 'Guns N Roses', 'Appetite for Destruction', 1987, 'Hard rock'),
+        (22, 'Bruce Springsteen', 'Born in the U.S.A.', 1984, 'Heartland rock'),
+        (21.6, 'ABBA', 'Gold: Greatest Hits', 1992, 'Pop'),
+        (21.1, 'Dire Straits', 'Brothers in Arms', 1985, 'Roots rock'),
+        (20.8, 'Santana', 'Supernatural', 1999, 'Latin Rock'),
+        (19.8, 'Madonna', 'The Immaculate Collection', 1990, 'Pop'),
+        (18.9, 'Pink Floyd', 'The Wall', 1979, 'Progressive rock'),
+        (18.3, 'The Beatles', 'Sgt Pepper''s Lonely Hearts Club Band', 'Rock'),
+        (17.0, 'Michael Jackson', 'Dangerous', 1991, 'New jack swing'),
+        (16.9, 'The Beatles', 'Abbey Road', 1969, 'Rock')
+;
